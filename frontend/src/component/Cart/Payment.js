08de90e4e603b48031923923fcb6ec97d26bdef5
Anuj Pandey <135626266+pandey03anuj@users.noreply.github.com>
2023-11-01 15:07:41 +0530

    Update Payment.js

--- frontend/src/component/Cart/Payment.js
+++ frontend/src/component/Cart/Payment.js
@@ -60,7 +60,7 @@ const Payment = () => {
         },
       };
       const { data } = await axios.post(
-        "/api/v1/payment/process",
+        `/api/v1/payment/process`,
         paymentData,
         config
       );
